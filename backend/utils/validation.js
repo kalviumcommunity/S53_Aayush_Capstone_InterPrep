7b89826ee81ff569e07f0c3730d928ff6d0eb489
aayushwrld <aayush.arora@kalvium.community>
2024-03-26 13:04:13 +0530

    Login Functionality + Password Hashing

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -4,7 +4,6 @@ module.exports.userValidation = Joi.object({
     username: Joi.string().required(),
     name: Joi.string().required(),
     password: Joi.string().required(),
-    interviews: Joi.array(),
     contact: Joi.object(),
     dateJoined: Joi.string()
 });
