98ce6714b46d646e1805e74018367da69f6d00ac
aayushwrld <aayush.arora@kalvium.community>
2024-05-03 13:50:09 +0530

    Made HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
 import AllRoutes from './components/routes/AllRoutes'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const customTheme = extendTheme({
+  breakpoints: {
+    xsm: '320px', // Example width for extra-small devices
+    sm: '480px',
+    md: '768px',
+    lg: '960px',
+    xl: '1200px',
+    '2xl': '1536px',
+  },
+});
 
 function App() {
 
   return (
     <>
+    <ChakraProvider theme={customTheme}>
       <AllRoutes />
+    </ChakraProvider>
     </>
   )
 }
--- frontend/src/components/routes/AllRoutes.jsx
+++ frontend/src/components/routes/AllRoutes.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Homepage from '../Homepage'
-import Test from '../Test'
+import Homepage from '../Pages/Homepage'
+import SignUp from '../Pages/SignUp'
+import Mainpage from '../Pages/Mainpage'
 
 function AllRoutes() {
   return (
     <>
     <Routes>
         <Route path='/' element={<Homepage/>}/>
-        <Route path='/test' element={<Test/>}/>
+        <Route path='/signup' element={<SignUp />}/>
+        <Route path='/browse' element={<Mainpage />}/>
     </Routes>
     </>
   )
