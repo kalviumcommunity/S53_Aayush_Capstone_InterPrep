5698673382176c51b5f4edd1fb6024e2861a8800
aayushwrld <aayush.arora@kalvium.community>
2024-12-09 00:03:07 +0530

    Dist

--- backend/dist/utils/wrapAsync.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const asyncHandler = (fn) => {
-    return (req, res, next) => {
-        fn(req, res, next).catch((err) => next(err));
-    };
-};
-exports.default = asyncHandler;
