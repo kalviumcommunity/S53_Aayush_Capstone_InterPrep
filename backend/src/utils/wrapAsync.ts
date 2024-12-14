7260bd52662e60687ec8e710d3cb1f6d243b0652
aayushwrld <aayushh2k@gmail.com>
2024-12-14 15:44:30 +0530

    backend move

--- backend/src/utils/wrapAsync.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) => {
-    return (req: Request, res: Response, next: NextFunction): void => {
-        fn(req, res, next).catch((err: any) => next(err));
-    };
-};
-
-export default asyncHandler;
